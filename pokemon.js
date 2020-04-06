74718da039f864b866c3e395afbef5b28ee8a066
Terry Chiem <terrychiem@gmail.com>
2020-04-06 16:36:55 -0700

    complete further study w/o styling

--- pokemon.js
+++ pokemon.js
@@ -9,40 +9,45 @@ async function getRandomPokemon(){
     );
   }
   let response = await Promise.all(requestArray);
-  let pokemon1 = response[0].data.name;
-  let pokemon2 = response[1].data.name;
-  let pokemon3 = response[2].data.name;
 
   let responseSpecies = await Promise.all([
     axios.get(response[0].data.species.url),
     axios.get(response[1].data.species.url),
     axios.get(response[2].data.species.url)
-
   ]);
-  
-  let pokemon1Text =  responseSpecies[0].data.flavor_text_entries.find(function(entry){
-    return entry.language.name === "en";
 
+  // collect names
+  let pokemonNames = response.map(pokemon => pokemon.data.name);
+
+  // collect english flavor text
+  let pokemonText = responseSpecies.map((resp) => {
+    let englishText = resp.data.flavor_text_entries.find(entry => entry.language.name === "en")
+    return englishText.flavor_text;
   });
 
-  let pokemon2Text =  responseSpecies[1].data.flavor_text_entries.find(function(entry){
-    return entry.language.name === "en";
+  // collect pokemon sprites
+  let pokemonImages = response.map(pokemon => pokemon.data.sprites.front_default);
 
-  });
+  console.log(pokemonNames);
+  console.log(pokemonText);
+  console.log(pokemonImages);
 
-  let pokemon3Text =  responseSpecies[2].data.flavor_text_entries.find(function(entry){
-    return entry.language.name === "en";
-
-  });
- 
-
-  console.log(pokemon1,pokemon1Text.flavor_text);
-  console.log(pokemon2,pokemon2Text.flavor_text);
-  console.log(pokemon3,pokemon3Text.flavor_text);
-
-
-
-
-   
-
+  // append to results div
+  for (let i = 0; i < 3; i++) {
+    let pokemonHTML = createPokemonHTML(pokemonNames[i], pokemonText[i], pokemonImages[i]);
+    $('#results').append(pokemonHTML);
+  }
 }
+
+function createPokemonHTML(name, flavorText, img) {
+  return $(`
+    <div>
+      <h3>${name}</h3>
+      <img src="${img}"></img>
+      <p>${flavorText}</p>
+    </div>
+  `)
+}
+
+
+$('#pokemon-btn').on('click', getRandomPokemon);
